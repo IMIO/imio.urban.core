e718b8b0d53f0d5d082d8f8886d2f322ea2794d2
Simon Delcourt <delcourt.simon@gmail.com>
2020-06-30 18:00:31 +0200

    Add base folder structure for Parcel type.

---
+++ src/imio/urban/core/contents/parcel/tests/test_parcel.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Setup/installation tests for Parcel."""
+
+from imio.urban.core.testing import IntegrationTestCase
+from plone import api
+
+
+class TestInstall(IntegrationTestCase):
+    """Test installation of imio.urban.core into Plone."""
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        self.types_tool = api.portal.get_tool('portal_types')
+
+    def test_Parcel_type_registered(self):
+        """Test if Parcel type is regsitered in portal_types """
+        self.assertTrue(self.types_tool.get('Parcel'))
