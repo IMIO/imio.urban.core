a8095c9e2c471bee08f9eb47b13ce25d1bb39491
Simon Delcourt <delcourt.simon@gmail.com>
2020-07-02 09:11:13 +0200

    Move iParcel unindexing to global indexes. Test parcellingTerm title update when updating a parcel.

--- src/imio/urban/core/contents/parcel/tests/test_parcel.py
+++ src/imio/urban/core/contents/parcel/tests/test_parcel.py
@@ -9,6 +9,9 @@ from imio.urban.core.testing import IntegrationTestCase
 from plone.app.testing import login
 from plone import api
 
+from zope.event import notify
+from zope.lifecycleevent import ObjectModifiedEvent
+
 import transaction
 import unittest2 as unittest
 
@@ -38,6 +41,7 @@ class TestParcel(unittest.TestCase):
         portal = self.layer['portal']
         self.portal = portal
         self.portal_urban = portal.portal_urban
+        self.parcellingterm = portal.urban.parcellings.objectValues()[0]
         default_user = self.layer.default_user
         login(self.portal, default_user)
         # create a test CODT_BuildLicence
@@ -54,7 +58,7 @@ class TestParcel(unittest.TestCase):
             api.content.delete(self.licence)
         transaction.commit()
 
-    def test_Parcel_unindexed_from_catalog(self):
+    def test_parcels_unindexed_from_catalog(self):
         catalog = api.portal.get_tool('portal_catalog')
         licence = self.licence
         parcel = api.content.create(
@@ -65,40 +69,53 @@ class TestParcel(unittest.TestCase):
         self.assertEqual(len(parcel_brains), 0)
 
     def test_parcel_indexing_on_licence(self):
+        self._test_parcel_indexing_on_container(self.licence)
+
+    def test_parcel_indexing_on_parcellingTerm(self):
+        self._test_parcel_indexing_on_container(self.parcellingterm)
+
+    def _test_parcel_indexing_on_container(self, container):
         catalog = api.portal.get_tool('portal_catalog')
-        licence = self.licence
-        licence_id = self.licence.id
-        licence_brain = catalog(id=licence_id)[0]
-        # so far, the index should be empty as  this licence contains no parcel
-        self.assertFalse(licence_brain.parcelInfosIndex)
+        container_id = container.id
+        container_brain = catalog(id=container_id)[0]
+        # so far, the index should be empty as  this container contains no parcel
+        self.assertFalse(container_brain.parcelInfosIndex)
 
         # add a parcel1, the index should now contain this parcel reference
-        api.content.create(
-            container=licence, type='Parcel', id='parcel1',
+        parcel_1 = api.content.create(
+            container=container, type='Parcel', id='parcel1',
             division='A', section='B', radical='6', exposant='D'
         )
-        parcel_1 = licence.parcel1
-        licence_brain = catalog(id=licence_id)[0]
-        self.assertIn(parcel_1.get_capakey(), licence_brain.parcelInfosIndex)
+        container_brain = catalog(id=container_id)[0]
+        self.assertIn(parcel_1.get_capakey(), container_brain.parcelInfosIndex)
 
         # add a parcel2, the index should now contain the two parcel references
-        api.content.create(
-            container=licence, type='Parcel', id='parcel2',
+        parcel_2 = api.content.create(
+            container=container, type='Parcel', id='parcel2',
             division='AA', section='B', radical='69', exposant='E'
         )
-        parcel_2 = licence.parcel2
-        licence_brain = catalog(id=licence_id)[0]
-        self.assertIn(parcel_1.get_capakey(), licence_brain.parcelInfosIndex)
-        self.assertIn(parcel_2.get_capakey(), licence_brain.parcelInfosIndex)
+        container_brain = catalog(id=container_id)[0]
+        self.assertIn(parcel_1.get_capakey(), container_brain.parcelInfosIndex)
+        self.assertIn(parcel_2.get_capakey(), container_brain.parcelInfosIndex)
 
-        # we remove parcel1, the ref of parcel2 should be the only remaining
-        # one, the index
-        licence.manage_delObjects(['parcel1'])
-        licence_brain = catalog(id=licence_id)[0]
-        self.assertNotIn(parcel_1.get_capakey(), licence_brain.parcelInfosIndex)
-        self.assertIn(parcel_2.get_capakey(), licence_brain.parcelInfosIndex)
+        # we remove parcel1, parcel2 capakey should be the only remaining
+        # on the index
+        api.content.delete(parcel_1)
+        container_brain = catalog(id=container_id)[0]
+        self.assertNotIn(parcel_1.get_capakey(), container_brain.parcelInfosIndex)
+        self.assertIn(parcel_2.get_capakey(), container_brain.parcelInfosIndex)
 
-    def test_parcel_events(self):
+        # modify parcel2 capakey, the index should be updated on the container
+        old_capakey = parcel_2.get_capakey()
+        parcel_2.puissance = '69'
+        self.assertNotEqual(old_capakey, parcel_2.get_capakey())
+        notify(ObjectModifiedEvent(parcel_2))
+        container_brain = catalog(id=container_id)[0]
+        self.assertIn(parcel_2.get_capakey(), container_brain.parcelInfosIndex)
+        # remove parcel_2 to not impact other tests
+        api.content.delete(parcel_2)
+
+    def test_parcel_event_set_authenticity(self):
         licence = self.licence
         parcel = api.content.create(
             container=licence, type='Parcel', id='parcel1',
@@ -119,7 +136,7 @@ class TestParcel(unittest.TestCase):
         self.assertEqual(view_result, licence.absolute_url())
 
     def test_divisionCode_field(self):
-        licence = self.licences[0]
+        licence = self.licence
         parcel = api.content.create(
             container=licence, type='Parcel', id='parcel1',
             division='69696', section='B', radical='6', exposant='D'
@@ -127,3 +144,31 @@ class TestParcel(unittest.TestCase):
         # divisionCode should always return the value contained in the field division.
         self.assertEqual(parcel.getDivisionCode(), parcel.getDivision())
         self.assertEqual(parcel.divisionCode, parcel.getDivision())
+
+    def test_parcellingTerm_title_update(self):
+        parcelling = self.parcellingterm
+        self.assertEquals(parcelling.Title(), 'Lotissement 1 (Andr\xc3\xa9 Ledieu - 01/01/2005)')
+        # after adding a parcel1, title should be updated with the base
+        # references  of this parcel (here:  A, B, C but not D)
+        api.content.create(container=parcelling, type='Parcel', id='parcel1', division='A', section='B', radical='6', exposant='D')
+        self.assertEquals(parcelling.Title(), 'Lotissement 1 (Andr\xc3\xa9 Ledieu - 01/01/2005 - "A B 6")')
+
+        # after adding a parcel2 with the same base refs, the title
+        # should not change
+        api.content.create(container=parcelling, type='Parcel', id='parcel2', division='A', section='B', radical='6', exposant='E')
+        self.assertEquals(parcelling.Title(), 'Lotissement 1 (Andr\xc3\xa9 Ledieu - 01/01/2005 - "A B 6")')
+
+        # after adding a parcel3 with different base refs, the title
+        # should be updated
+        parcel_3 = api.content.create(container=parcelling, type='Parcel', id='parcel3', division='AA', section='BB', radical='69', exposant='D')
+        self.assertEquals(parcelling.Title(), 'Lotissement 1 (Andr\xc3\xa9 Ledieu - 01/01/2005 - "AA BB 69", "A B 6")')
+
+        # we remove parcel1 and parcel2, title should change to only
+        # keep the base refs of parcel3
+        parcelling.manage_delObjects(['parcel1', 'parcel2'])
+        self.assertEquals(parcelling.Title(), 'Lotissement 1 (Andr\xc3\xa9 Ledieu - 01/01/2005 - "AA BB 69")')
+
+        # modify parcel3, parcelling title should be updated
+        parcel_3.division = 'AAA'
+        notify(ObjectModifiedEvent(parcel_3))
+        self.assertEquals(parcelling.Title(), 'Lotissement 1 (Andr\xc3\xa9 Ledieu - 01/01/2005 - "AAA BB 69")')
