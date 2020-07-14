bbfb26309a9ee137267d94911bc7f46b82b88365
Julien Jaumotte <julien.jaumotte@imio.be>
2020-07-14 17:09:00 +0200

    Add to Dexterity ParcellingTerm new portal_type schema attributes

--- src/imio/urban/core/contents/parcellingterm/content.py
+++ src/imio/urban/core/contents/parcellingterm/content.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from imio.urban.core import _
+
+from plone.autoform import directives as form
 from plone.dexterity.content import Item
 from plone.supermodel import model
 
+from z3c.form.browser.text import TextWidget
+from z3c.form.browser.textarea import TextAreaWidget
+
+from zope import schema
 from zope.interface import implementer
 
 
@@ -11,13 +18,63 @@ class IParcellingTerm(model.Schema):
     ParcellingTerm dexterity schema.
     """
 
+    form.widget('title', TextWidget)
+    title = schema.TextLine(
+        title=_(u'urban_label_title'),
+        required=False,
+    )
+
+    form.widget('label', TextWidget)
+    label = schema.TextLine(
+        title=_(u'urban_label_label'),
+        required=True,
+    )
+
+    form.widget('subdividerName', TextWidget)
+    subdividerName = schema.TextLine(
+        title=_(u'urban_label_subdividerName'),
+        required=True,
+    )
+
+    form.widget('authorizationDate')
+    authorizationDate = schema.Datetime(
+        title=_(u'urban_label_authorizationDate'),
+        required=False,
+    )
+
+    form.widget('approvalDate')
+    approvalDate = schema.Datetime(
+        title=_(u'urban_label_approvalDate'),
+        required=False,
+    )
+
+    form.widget('communalReference', TextWidget)
+    communalReference = schema.TextLine(
+        title=_(u'urban_label_CommunalReference'),
+        required=False,
+    )
+
+    form.widget('DGO4Reference', TextWidget)
+    DGO4Reference = schema.TextLine(
+        title=_(u'urban_label_DGO4Reference'),
+        required=False,
+    )
+
+    form.widget('numberOfParcels')
+    numberOfParcels = schema.Int(
+        title=_(u'urban_label_numberOfParcels'),
+        required=True,
+    )
+
+    form.widget('changesDescription', TextAreaWidget)
+    changesDescription = schema.TextLine(
+        title=_(u'urban_label_changesDescription'),
+        required=False,
+    )
+
 
 @implementer(IParcellingTerm)
 class ParcellingTerm(Item):
     """
     ParcellingTerm dexterity class.
     """
-
-    def Title(self):
-        """
-        """
