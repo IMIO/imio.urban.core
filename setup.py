0fc1f517dd8cecbe709096e4ab4909249ce3287f
Simon Delcourt <delcourt.simon@gmail.com>
2020-07-28 14:01:01 +0200

    URB-2066. Complete dependencies

--- setup.py
+++ setup.py
@@ -41,8 +41,10 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'collective.z3cform.datagridfield',
         'plone.api',
         'plone.app.referenceablebehavior',
+        'plone.formwidget.masterselect',
         'setuptools',
     ],
     extras_require={
