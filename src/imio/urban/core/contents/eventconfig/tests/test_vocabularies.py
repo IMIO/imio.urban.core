8e5584cb8d832f9c769cb135a96662e4b8359348
Simon Delcourt <delcourt.simon@gmail.com>
2020-07-28 14:01:31 +0200

    URB-2066. Test eventPortalTypes vocabulary.

--- src/imio/urban/core/contents/eventconfig/tests/test_vocabularies.py
+++ src/imio/urban/core/contents/eventconfig/tests/test_vocabularies.py
@@ -22,4 +22,40 @@ class TestInstall(IntegrationTestCase):
         vocabulary = queryUtility(IVocabularyFactory, 'urban.vocabularies.event_portaltypes')
         self.assertTrue(vocabulary)
         voc = vocabulary(self.portal)
-        import ipdb; ipdb.set_trace()
+        # when called out of any licenceConfig, should return all UrbanEvent
+        # portal types
+        expected_types = set([
+            'UrbanEvent',
+            'UrbanEventInquiry',
+            'UrbanEventAnnouncement',
+            'UrbanEventInspectionReport',
+            'UrbanEventFollowUp',
+            'UrbanEventOpinionRequest',
+        ])
+        self.assertEquals(set(voc.by_value), expected_types)
+        # when called on a licenceConfig should only return the types in
+        # the allowed contenttypes of the licence
+        voc = vocabulary(self.portal.portal_urban.codt_buildlicence.urbaneventtypes)
+        expected_types = set([
+            'UrbanEvent',
+            'UrbanEventInquiry',
+            'UrbanEventAnnouncement',
+            'UrbanEventOpinionRequest',
+        ])
+        self.assertEquals(set(voc.by_value), expected_types)
+
+        voc = vocabulary(self.portal.portal_urban.buildlicence.urbaneventtypes)
+        expected_types = set([
+            'UrbanEvent',
+            'UrbanEventInquiry',
+            'UrbanEventOpinionRequest',
+        ])
+        self.assertEquals(set(voc.by_value), expected_types)
+
+        voc = vocabulary(self.portal.portal_urban.inspection.urbaneventtypes)
+        expected_types = set([
+            'UrbanEvent',
+            'UrbanEventInspectionReport',
+            'UrbanEventFollowUp',
+        ])
+        self.assertEquals(set(voc.by_value), expected_types)
